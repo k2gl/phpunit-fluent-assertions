61930519664630f1b34c3d1b172d2aaaacff4f12
l0gic <l0gic@ya.ru>
2026-01-22 05:55:19 +0500

    Fix PHPDoc return type in StringAssertions for matchesRegularExpression

--- src/Traits/StringAssertions.php
+++ src/Traits/StringAssertions.php
@@ -27,7 +27,7 @@ trait StringAssertions
      * @param string $pattern The regular expression pattern to match against.
      * @param string $message Optional custom error message.
      *
-     * @return StringAssertions|FluentAssertions Enables fluent chaining of assertion methods.
+     * @return self Enables fluent chaining of assertion methods.
      */
     public function matchesRegularExpression(string $pattern, string $message = ''): self
     {
